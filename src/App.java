2fd0fac2c0eb495f5a2fa4cbe68684c9ea9e23eb
Allen-work12 <1225231154@qq.com>
2025-02-24 14:53:57 +0800

    pizza

--- src/App.java
+++ src/App.java
@@ -1,7 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        LuckinCoffee luckcoffee = new LuckinCoffee();
-        luckcoffee.takeOrder("1234", "2 lattes", 20.00);
-        
+        SliceOHeaven sliceOHeaven = new SliceOHeaven();
+        sliceOHeaven.storeName = "Slice-o-Heaven";
+        sliceOHeaven.storeAddress = "123 Pizza Street";
+        sliceOHeaven.storeEmail = "contact@sliceoheaven.com";
+        sliceOHeaven.storePhone = 1234567890L;
+        sliceOHeaven.storeMenu = "Pizza, Sides, Drinks";
+        sliceOHeaven.pizzaIngredients = "Cheese, Tomato Sauce, Pepperoni";
+        sliceOHeaven.pizzaPrice = 12.99;
+        sliceOHeaven.sides = "Garlic Bread, Salad";
+        sliceOHeaven.drinks = "Soda, Water";
+
+        sliceOHeaven.takeOrder("1234", 15.99);
     }
 }
